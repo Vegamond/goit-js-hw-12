76a6f3a7a4edf7b6816de74a7c8ba1d8c3eb6e5c
Vega <alexkarpytskyi@gmail.com>
2025-11-02 19:49:12 +0200

    build: test

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite';
-import { glob } from 'glob';
-import injectHTML from 'vite-plugin-html-inject';
-import FullReload from 'vite-plugin-full-reload';
-import SortCss from 'postcss-sort-media-queries';
-
 export default defineConfig(({ command }) => {
   return {
+    // 👇 ДОДАЙ ОЦЕ
+    base: '/goit-js-hw-12/',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
@@ -16,22 +12,14 @@ export default defineConfig(({ command }) => {
         input: glob.sync('./src/*.html'),
         output: {
           manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
+            if (id.includes('node_modules')) return 'vendor';
           },
-          entryFileNames: chunkInfo => {
-            if (chunkInfo.name === 'commonHelpers') {
-              return 'commonHelpers.js';
-            }
-            return '[name].js';
-          },
-          assetFileNames: assetInfo => {
-            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
-              return '[name].[ext]';
-            }
-            return 'assets/[name]-[hash][extname]';
-          },
+          entryFileNames: chunkInfo =>
+            chunkInfo.name === 'commonHelpers' ? 'commonHelpers.js' : '[name].js',
+          assetFileNames: assetInfo =>
+            assetInfo.name && assetInfo.name.endsWith('.html')
+              ? '[name].[ext]'
+              : 'assets/[name]-[hash][extname]',
         },
       },
       outDir: '../dist',
@@ -40,9 +28,7 @@ export default defineConfig(({ command }) => {
     plugins: [
       injectHTML(),
       FullReload(['./src/**/**.html']),
-      SortCss({
-        sort: 'mobile-first',
-      }),
+      SortCss({ sort: 'mobile-first' }),
     ],
   };
 });
